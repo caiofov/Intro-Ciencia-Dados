343cf1338f8ce6233c5b5caef788f0866d34d5c9
Caio Oliveira <cfoviana@alu.ufc.br>
2024-03-30 15:44:29 -0300

    parte 2

--- trabalho-01/trabalho_01/parte_2.py
+++ trabalho-01/trabalho_01/parte_2.py
@@ -1,15 +1,41 @@
+# %%
+
 # Parte 2: Regressão com o Wine Quality Dataset
 
-from sklearn import datasets # type: ignore[import-untyped]
+# mypy: disable-error-code="import-untyped"
+from sklearn import datasets
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from trabalho_01.utils import print_result
+
+# %%
 
 # Carregue o Wine Quality dataset
-datasets.load_wine()
+wine = datasets.load_wine()
 
+# %%
+# Divisão do conjunto de dados
 
-# Realize uma análise exploratória básica para entender as características dos dados.
+X, y = datasets.make_regression(random_state=0)
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, train_size=0.7, random_state=0)
 
-# Divida os dados em conjuntos de treino e teste.
+# %%
+# LinearRegression
+lr = LinearRegression()
+# treino
+lr.fit(X_train, y_train)
+# resultado
+print_result(lr.score(X_test, y_test), lr.predict(X) == y)
 
+# %%
+# RandomForestRegressor
 
-# Treine e avalie modelos usando LinearRegression e RandomForestRegressor. Ajuste os parâmetros conforme necessário e discuta a performance dos modelos.
+rfr = RandomForestRegressor()
+# treino
+rfr.fit(X_train, y_train)
+# resultado
+print_result(rfr.score(X_test, y_test), rfr.predict(X) == y)
+
+# %%
