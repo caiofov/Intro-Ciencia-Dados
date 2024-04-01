a754c70aca557d06b991e8c1aa0643e617465ea4
Caio Oliveira <cfoviana@alu.ufc.br>
2024-04-01 08:40:36 -0300

    trab 1 - parte 3

--- trabalho-01/trabalho_01/parte_3.py
+++ trabalho-01/trabalho_01/parte_3.py
@@ -1,9 +1,40 @@
+# %%
 # Parte 3: Classificação com o Digits Dataset
-from sklearn import datasets # type: ignore[import-untyped]
+# mypy: disable-error-code="import-untyped"
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from trabalho_01.utils import print_result
 
+# %%
 # Carregue o Digits dataset usando sklearn.datasets.load_digits().
 datasets.load_digits()
+# %%
+# Divisão do conjunto de dados
 
-# Divida os dados em conjuntos de treino e teste.
+X, y = datasets.make_classification(random_state=0)
 
-# Treine e avalie modelos usando KNeighborsClassifier e SVC. Explore diferentes valores de parâmetros e compare a acurácia dos modelos.
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, train_size=0.7, random_state=0)
+
+
+# %%
+# SVC
+svc = SVC()
+
+# treino
+svc.fit(X_train, y_train)
+
+# resultado
+print_result(svc.score(X_test, y_test), svc.predict(X) == y)
+
+# %%
+# KNeighborsClassifier
+knc = KNeighborsClassifier()
+
+# treino
+knc.fit(X_train, y_train)
+
+# resultado
+print_result(knc.score(X_test, y_test), knc.predict(X) == y)
+# %%
